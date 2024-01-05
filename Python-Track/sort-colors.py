d089aea3fcf37473ef7aadb3f5de4a0f469faed2
MubarakAd <150241158+MubarakAd@users.noreply.github.com>
2024-01-06 01:31:44 +0300

    Added solution for Sort Colors

---
+++ Python-Track/sort-colors.py
@@ -0,0 +1,11 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if nums[j]>nums[i]:
+                    nums[i],nums[j]=nums[j],nums[i]
+        
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
