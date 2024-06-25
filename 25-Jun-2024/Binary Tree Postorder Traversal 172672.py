fde09006b830bcd8a39a495203b212009b94519d
MubarakAd <150241158+MubarakAd@users.noreply.github.com>
2024-06-25 22:22:46 +0300

    Created 'Binary Tree Postorder Traversal' by A2SV Hub

---
+++ 25-Jun-2024/Binary Tree Postorder Traversal 172672.py
@@ -0,0 +1,21 @@
+# Problem: Binary Tree Postorder Traversal - https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+
+
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+
+        stack = [(root, False)]
+        res = []
+        while stack:
+
+            currentNode, visited = stack.pop()
+            if not currentNode:
+                continue
+            if visited:
+                res.append(currentNode.val)
+            else:
+                stack.append((currentNode, True))
+                stack.append((currentNode.right, False))
+                stack.append((currentNode.left, False))
+        return res
+        
